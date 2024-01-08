ec234b9aa2992b06d72060e9c6f6b22884e5cb6d
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2024-01-08 08:35:10 +0530

    Updated Skeleton CSS Bottom Nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script lang="ts">
+  import BottomNav from "$lib/home/BottomNav.svelte";
+  import "../app.pcss";
+  import { ModeWatcher } from "mode-watcher";
+</script>
+
+<ModeWatcher />
+<slot />
+<BottomNav />
